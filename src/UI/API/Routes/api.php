307fdf6e888b1f4d660556669fcd9c50e2fa5965
daurensky <dkambarov17@gmail.com>
2024-03-12 14:36:13 +0500

    chore: remove unused route

--- src/UI/API/Routes/api.php
+++ src/UI/API/Routes/api.php
@@ -4,4 +4,3 @@ use AdminKit\Products\UI\API\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{id}', [ProductController::class, 'show']);
