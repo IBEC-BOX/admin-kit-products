1cc942e083e6f3de9b60302acbdd10eb10c43745
Anastas Mironov <ast@l80.ru>
2024-07-05 02:01:55 +0900

    fix: laravel-data version ^4.5

--- src/UI/API/DTO/ProductDTO.php
+++ src/UI/API/DTO/ProductDTO.php
@@ -4,10 +4,13 @@ namespace AdminKit\Products\UI\API\DTO;
 
 use AdminKit\Products\Models\Product;
 use Illuminate\Support\Collection;
+use Spatie\LaravelData\Concerns\WithDeprecatedCollectionMethod;
 use Spatie\LaravelData\Data;
 
 class ProductDTO extends Data
 {
+    use WithDeprecatedCollectionMethod;
+
     public function __construct(
         public string $title,
         public string $text,
